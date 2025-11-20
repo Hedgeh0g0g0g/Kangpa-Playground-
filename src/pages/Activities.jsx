64f2e4897ebdadfc0328023619d534e9dcac23f6
akixrrr <fungusbad2550@gmail.com>
2025-11-20 17:35:02 +0700

    1.2

---
+++ src/pages/Activities.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Activities() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-6">
+            <h1 className="text-3xl font-bold mb-4">โปรเจกต์</h1>
+            <p>นี่คือหน้าสำหรับโปรเจกต์ทั้งหมด</p>
+        </div>
+    );
+}
